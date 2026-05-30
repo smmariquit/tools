055f775e0245c4ad08c8d340ee5056aa3e3e4527
smmariquit <smmariquit@up.edu.ph>
2026-05-31 00:05:41 +0800

    chore: use native script tag for AdSense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,13 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Replace the client ID with your actual Google AdSense publisher ID */}
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9785940474424207"
-          crossOrigin="anonymous"
-          strategy="lazyOnload"
-        />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9785940474424207" crossOrigin="anonymous"></script>
       </head>
       <body className={inter.className}>
         <PostHogProvider>
