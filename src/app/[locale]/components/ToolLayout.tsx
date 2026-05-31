e865bbd0f87dcc2fc95419f0f11c0015e07f7360
smmariquit <smmariquit@up.edu.ph>
2026-05-31 17:53:13 +0800

    style: maximize desktop screen real estate to 1200px max-width

--- src/app/[locale]/components/ToolLayout.tsx
+++ src/app/[locale]/components/ToolLayout.tsx
@@ -5,5 +5,9 @@ type ToolLayoutProps = {
 };
 
 export default function ToolLayout({ children }: ToolLayoutProps) {
-	return <div style={{ maxWidth: "800px", margin: "0 auto" }}>{children}</div>;
+	return (
+		<div style={{ maxWidth: "1200px", margin: "0 auto", width: "100%" }}>
+			{children}
+		</div>
+	);
 }
